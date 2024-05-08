319c8e81c2e51c6f55c39eb245239c61ff72f121
minqukanq <kang.mingu94@gmail.com>
2024-05-08 14:29:07 +0900

    data provider refactoring

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -5,16 +5,16 @@ from data_provider.data_loader import CryptocurrencyDataset
 dataset_dict = {"CryptocurrencyDataset": CryptocurrencyDataset}
 
 
-def data_provider(args, shuffle):
+def data_provider(args, flag):
     data = dataset_dict[args.data]
-    shuffle = False if shuffle == "test" else True
+    shuffle = False if flag == "test" else True
     batch_size = args.batch_size
 
     if args.task_name == "forecasting":
         dataset = data(
             root_path=args.root_path,
             data_path=args.data_paths,
-            flag=shuffle,
+            flag=flag,
             size=[args.seq_len, args.pred_len, args.label_len],
             features=args.features,
             test_start_date=args.test_start_date,
@@ -22,7 +22,7 @@ def data_provider(args, shuffle):
             freq=args.freq,
             scaler=args.scaler,
         )
-        print(shuffle, len(data))
+        print(flag, len(data))
         drop_last = True
         data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=args.num_workers, drop_last=drop_last)
         return dataset, data_loader
