d61128ec2586423d5f480f2b55e2081a1ad412df
minqukanq <kang.mingu94@gmail.com>
2024-05-08 14:27:14 +0900

    update data provider

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -5,31 +5,24 @@ from data_provider.data_loader import CryptocurrencyDataset
 dataset_dict = {"CryptocurrencyDataset": CryptocurrencyDataset}
 
 
-def data_provider(args, flag):
+def data_provider(args, shuffle):
     data = dataset_dict[args.data]
+    shuffle = False if shuffle == "test" else True
+    batch_size = args.batch_size
 
-    if flag == "test":
-        shuffle = False
-        batch_size = 1  # bsz=1 for evaluation
-    elif flag == "valid":
-        shuffle = False
-        batch_size = args.batch_size
-    else:
-        shuffle = True
-        batch_size = args.batch_size  # bsz for train
-
-    dataset = data(
-        root_path=args.root_path,
-        data_path=args.data_paths,
-        flag=flag,
-        size=[args.seq_len, args.pred_len, args.label_len],
-        features=args.features,
-        test_start_date=args.test_start_date,
-        target=args.target,
-        freq=args.freq,
-        scaler=args.scaler,
-    )
-    print(flag, len(data))
-    drop_last = True
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=args.num_workers, drop_last=drop_last)
-    return dataset, data_loader
+    if args.task_name == "forecasting":
+        dataset = data(
+            root_path=args.root_path,
+            data_path=args.data_paths,
+            flag=shuffle,
+            size=[args.seq_len, args.pred_len, args.label_len],
+            features=args.features,
+            test_start_date=args.test_start_date,
+            target=args.target,
+            freq=args.freq,
+            scaler=args.scaler,
+        )
+        print(shuffle, len(data))
+        drop_last = True
+        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=args.num_workers, drop_last=drop_last)
+        return dataset, data_loader
