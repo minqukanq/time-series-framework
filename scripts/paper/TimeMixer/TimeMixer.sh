6293bb261592fd3dc21b25767bd45e48729bd7da
minqukanq <kang.mingu94@gmail.com>
2024-05-23 22:30:42 +0900

    update timemixer args

--- scripts/paper/TimeMixer/TimeMixer.sh
+++ scripts/paper/TimeMixer/TimeMixer.sh
@@ -3,69 +3,69 @@ model_name=TimeMixer
 for market in BTC ETH XRP
 do
 
-for pred_len in 6 12 24 48 96
-do
-python run.py \
-    --is_training 1 \
-    --use_wandb \
-    --data_paths upbit/KRW-$market'_MINUTES_30'.csv \
-    --model $model_name \
-    --id $market'_30m_pred'$pred_len'_2' \
-    --freq h \
-    --scaler S \
-    --pred_len $pred_len \
-    --e_layers 2 \
-    --d_model 16 \
-    --d_ff 32 \
-    --down_sampling_layers 3 \
-    --down_sampling_window 2 \
-    --down_sampling_method avg \
-    --des 'Paper '$market' Min 30' \
-    --gpu 0
-done
+# for pred_len in 6 12 24 48 96
+# do
+# python run.py \
+#     --is_training 1 \
+#     --use_wandb \
+#     --data_paths upbit/KRW-$market'_MINUTES_30'.csv \
+#     --model $model_name \
+#     --id $market'_30m_pred'$pred_len'_2' \
+#     --freq h \
+#     --scaler S \
+#     --pred_len $pred_len \
+#     --e_layers 2 \
+#     --d_model 16 \
+#     --d_ff 32 \
+#     --down_sampling_layers 3 \
+#     --down_sampling_window 2 \
+#     --down_sampling_method avg \
+#     --des 'Paper '$market' Min 30' \
+#     --gpu 0
+# done
 
-for pred_len in 6 12 24 48 96
-do
-python run.py \
-    --is_training 1 \
-    --use_wandb \
-    --data_paths upbit/KRW-$market'_HOUR'.csv \
-    --model $model_name \
-    --id $market'_1h_pred'$pred_len'_2' \
-    --freq h \
-    --scaler S \
-    --pred_len $pred_len \
-    --e_layers 2 \
-    --d_model 16 \
-    --d_ff 32 \
-    --down_sampling_layers 3 \
-    --down_sampling_window 2 \
-    --down_sampling_method avg \
-    --des 'Paper '$market' Hour 1' \
-    --gpu 0
-done
+# for pred_len in 6 12 24 48 96
+# do
+# python run.py \
+#     --is_training 1 \
+#     --use_wandb \
+#     --data_paths upbit/KRW-$market'_HOUR'.csv \
+#     --model $model_name \
+#     --id $market'_1h_pred'$pred_len'_2' \
+#     --freq h \
+#     --scaler S \
+#     --pred_len $pred_len \
+#     --e_layers 2 \
+#     --d_model 16 \
+#     --d_ff 32 \
+#     --down_sampling_layers 3 \
+#     --down_sampling_window 2 \
+#     --down_sampling_method avg \
+#     --des 'Paper '$market' Hour 1' \
+#     --gpu 0
+# done
 
-for pred_len in 6 12 24 48 96
-do
-python run.py \
-    --is_training 1 \
-    --use_wandb \
-    --data_paths upbit/KRW-$market'_HOUR4'.csv \
-    --model $model_name \
-    --id $market'_4h_pred'$pred_len'_2' \
-    --freq h \
-    --scaler S \
-    --pred_len $pred_len \
-    --e_layers 2 \
-    --d_model 16 \
-    --d_ff 32 \
-    --down_sampling_layers 3 \
-    --down_sampling_window 2 \
-    --down_sampling_method avg \
-    --test_start_date '2023-05-16 01:00:00' \
-    --des 'Paper '$market' Hour 4' \
-    --gpu 0
-done
+# for pred_len in 6 12 24 48 96
+# do
+# python run.py \
+#     --is_training 1 \
+#     --use_wandb \
+#     --data_paths upbit/KRW-$market'_HOUR4'.csv \
+#     --model $model_name \
+#     --id $market'_4h_pred'$pred_len'_2' \
+#     --freq h \
+#     --scaler S \
+#     --pred_len $pred_len \
+#     --e_layers 2 \
+#     --d_model 16 \
+#     --d_ff 32 \
+#     --down_sampling_layers 3 \
+#     --down_sampling_window 2 \
+#     --down_sampling_method avg \
+#     --test_start_date '2023-05-16 01:00:00' \
+#     --des 'Paper '$market' Hour 4' \
+#     --gpu 0
+# done
 
 for pred_len in 3 6 12 24 36
 do
@@ -84,7 +84,7 @@ python run.py \
     --d_model 16 \
     --d_ff 32 \
     --down_sampling_layers 3 \
-    --down_sampling_window 2 \
+    --down_sampling_window 1 \
     --down_sampling_method avg \
     --test_start_date '2023-05-16 09:00:00' \
     --des 'Paper '$market' Day' \
